f005cccdab746a22e30aeeb1bc8e11a5895ce876
vttoteva25 <stu2201322010@uni-plovdiv.bg>
2023-01-29 22:44:09 +0200

    Add project files.

---
+++ Triplets/Program.cs
@@ -0,0 +1,62 @@
+﻿using Triplets;
+
+namespace Triplets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var triplets = new List<int[]>();
+            for (int i = 0; i < input[1]; i++)
+            {
+                triplets.Add(Console.ReadLine().Split(" ").Select(int.Parse).ToArray()); 
+            }
+
+            var (countOfConflictNodes, numbersOfConflictNodes) = CheckNodeForConflict(input[0], input[1], triplets);
+            Console.WriteLine(countOfConflictNodes.ToString());
+            Console.WriteLine(string.Join(" ", numbersOfConflictNodes));
+          
+        }
+        private static (int countOfConflictNodes, List<int> numbersOfConflictNodes) CheckNodeForConflict(int vertices, int countTriplets, List<int[]> triplets)
+        {
+            var countOfConflictNodes = 0;
+            var numbersOfConflictEdges = new List<int>();
+            int indexOfTripletRemoved = 0;
+
+            while (indexOfTripletRemoved < countTriplets)
+            {
+                var graph = CreateGraph(vertices, countTriplets, triplets, indexOfTripletRemoved);
+               
+                    var accessibleVertices = graph.BFS(1);
+                    if(accessibleVertices.Count != vertices)
+                    {
+                        countOfConflictNodes++;
+                        numbersOfConflictEdges.Add(indexOfTripletRemoved+1);
+                    }
+              
+                indexOfTripletRemoved++;
+            }
+
+            return (countOfConflictNodes, numbersOfConflictEdges);
+        }
+
+        private static UndirectedGraph CreateGraph(int vertices, int countTriplets, List<int[]> triplets, int index)
+        {
+            UndirectedGraph grаph = new UndirectedGraph(vertices);
+            
+            for (int i = 0; i < countTriplets; i++)
+            {
+                if (index == i)
+                    continue;
+                grаph.AddEdge(triplets[i][0], triplets[i][1]);
+                grаph.AddEdge(triplets[i][0], triplets[i][2]);
+                grаph.AddEdge(triplets[i][1], triplets[i][2]);
+            }
+
+            return grаph;
+        }
+    }
+
+   
+}
---
+++ Triplets/UndirectedGraph.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triplets
+{
+    internal class UndirectedGraph
+    {
+        private int vertices;
+        private int edges = 0;
+        private List<int>[] adjacencyLists;
+
+        public UndirectedGraph(int vertices)
+        {
+            this.vertices = vertices;
+
+            this.adjacencyLists = new List<int>[vertices + 1];
+            for (int i = 1; i < this.adjacencyLists.Length; i++)
+                this.adjacencyLists[i] = new List<int>();
+        }
+
+        public void AddEdge(int v, int w)
+        {          
+            if ((v > this.adjacencyLists.Length) || (w > this.adjacencyLists.Length))
+                throw new ArgumentException("Invalid node number specified.");
+
+            if (!this.adjacencyLists[v].Contains(w))
+            {
+                this.adjacencyLists[v].Add(w);
+                this.adjacencyLists[w].Add(v);
+
+                this.edges++;
+            }
+
+        }
+
+        public List<int> BFS(int vertice)
+        {
+            var result = new List<int>();
+            bool[] visited = new bool[vertices+1];
+            for (int i = 0; i < vertices; i++)
+                visited[i] = false;
+
+            LinkedList<int> queue = new LinkedList<int>();
+
+            visited[vertice] = true;
+            queue.AddLast(vertice);
+
+            while (queue.Any())
+            {
+                vertice = queue.First();
+                result.Add(vertice);
+                queue.RemoveFirst();
+              
+                List<int> list = adjacencyLists[vertice];
+
+                foreach (var value in list)
+                {
+                    if (!visited[value])
+                    {
+                        visited[value] = true;
+                        queue.AddLast(value);
+                    }
+                }
+            }
+            return result;
+        }       
+    }
+}
+
